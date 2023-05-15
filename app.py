12d2b884ee7332fd92d1376e2ed2a865adbcfb51
Bel <114267626+BA-BEL@users.noreply.github.com>
2023-05-15 19:32:17 +1000

    fixed up file formatting | proceeding to adapt index.html d3 functionality with real data

--- app.py
+++ app.py
@@ -20,17 +20,6 @@ from flask import   Flask,              \
                     make_response,      \
                     redirect
 
-#   Dom machine learning deps
-import nltk
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-from string import punctuation
-import re
-from nltk.corpus import stopwords
-
-
 
 ############# DATABASE SETUP #############
 
@@ -257,59 +246,6 @@ def stats():
 @app.route("/stats", methods = ["GET", "POST"])
 def map():
     return render_template("stats.html")
-
-#   TEST ROUTE FOR POST METHODS
-@app.route("/test", methods = ["GET", "POST"])
-def test():
-    return render_template("test.html")
-
-#####   DOM ROUTES
-
-@app.route('/dom')
-def dom():
-    return render_template('home.html')
-
-@app.route('/form')
-def my_form():
-    return render_template('form.html')
-
-@app.route('/form', methods=['POST'])
-def my_form_post():
-    stop_words = stopwords.words('english')
-    
-    #convert to lowercase
-    text1 = request.form['text1'].lower()
-    
-    text_final = ''.join(c for c in text1 if not c.isdigit())
-    
-    #remove punctuations
-    #text3 = ''.join(c for c in text2 if c not in punctuation)
-        
-    #remove stopwords    
-    processed_doc1 = ' '.join([word for word in text_final.split() if word not in stop_words])
-
-    sa = SentimentIntensityAnalyzer()
-    dd = sa.polarity_scores(text=processed_doc1)
-    compound = round((1 + dd['compound'])/2, 2)
-
-    return render_template('form.html', final=compound, text1=text_final,text2=dd['pos'],text5=dd['neg'],text4=compound,text3=dd['neu'])
-
-
-@app.route('/input-nltk-vader', methods=["GET", "POST"])
-def vader():
-    if request.method == "POST":
-        inp = request.form.get("inp")
-        sia = SentimentIntensityAnalyzer()
-        score = sia.polarity_scores(inp)
-
-        if score["neg"] != 0:
-            return render_template ('input_vader.html', message= "Negative 😵😵")
-        else:
-            return render_template('input_vader.html', message="Positive 🙂🙂")
-        
-    return render_template('input_vader.html')
-
-
 
 
 ############# INIT #############
